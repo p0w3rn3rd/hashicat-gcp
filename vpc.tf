d2fb2d4762b5e95728f898e82954f4a284115b3e
p0w3rn3rd <p0w3rn3rd@icloud.com>
2020-11-11 19:05:54 +0000

    added vpc module for gaurav

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,9 @@
 module "network" {
   source  = "app.terraform.io/p0w3rn3rd/network/google"
   version = "2.5.0"
-    subnets = [
+  network_name = "gaurav-network"
+  project_id = var.project
+  subnets = [
     {
       subnet_name   = "gaurav-subnet"
       subnet_ip     = "10.100.10.0/24"
